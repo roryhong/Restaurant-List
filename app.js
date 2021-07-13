904ca320c1bc7fe2015298dd24acc52c0a5c9e16
roryhong <hongst1995@gmail.com>
2021-07-13 14:32:12 +0800

    add method-override

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const Restaurants = require('./models/restaurant')
-const app = express()
-const port = 3000
 const mongoose = require('mongoose')
-const db = mongoose.connection
+const methodOverride = require('method-override')
+const Restaurants = require('./models/restaurant')
+const port = 3000
+const app = express()
 
+const db = mongoose.connection
 mongoose.connect('mongodb://localhost/restaurant-list' , { useNewUrlParser: true, useUnifiedTopology: true })
+
 app.engine('hbs', exphbs({defaultLayout : 'main', extname : '.hbs'}))
 app.set('view engine','hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded( { extended: true }))
+app.use(methodOverride('_method'))
 
 db.on('error', () => {
     console.log('mongodb error!')
@@ -87,7 +90,7 @@ app.get('/restaurants/:id/edit', (req , res) => {
 })
 
 //edit : 修改資料
-app.post('/restaurants/:id/edit', (req , res) => {
+app.put('/restaurants/:id', (req , res) => {
     const { name , name_en , category , image , location , phone , google_map , rating , description } = req.body
     const id = req.params.id
     return Restaurants.findById(id)
@@ -108,7 +111,7 @@ app.post('/restaurants/:id/edit', (req , res) => {
 })
 
 //delete
-app.post('/restaurants/:id/delete' ,(req , res) => {
+app.delete('/restaurants/:id' ,(req , res) => {
     const id = req.params.id
     return Restaurants.findById(id)
       .then(restaurant => restaurant.remove())
